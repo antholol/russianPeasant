d3bc01f9ad645556ff1029af60a3ffda23f15939
antholol <86118257+antholol@users.noreply.github.com>
2025-10-03 21:14:11 +0700

    Add project files.

---
+++ russianPeasant.vb
@@ -0,0 +1,31 @@
+Imports System
+
+Module russianPeasant
+    Sub Main(args As String())
+        russianPeasant(67, 69)
+    End Sub
+    Function isOdd(ByVal number As Integer) As Boolean
+        Try
+            If (number - 1) Mod 2 = 0 Then
+                Return True
+            Else
+                Return False
+            End If
+        Catch oopsie As Exception
+            Console.WriteLine("Error: Coundn't parse input")
+            Return False
+        End Try
+    End Function
+    Function russianPeasant(ByVal num1multiply As Integer, ByVal num2toMultiply As Integer) As Integer
+        Dim sum, temp As Integer
+        sum = 0
+        temp = 0
+        If num1multiply < num2toMultiply Then
+            temp = num1multiply
+            num1multiply = num2toMultiply
+            num2toMultiply = temp
+        End If
+
+        Return -1
+    End Function
+End Module
